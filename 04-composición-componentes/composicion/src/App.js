e3e27665fdfc2b856533073b27b0876ad870c286
Rods <rodolfo.perez01@iest.edu.mx>
2022-09-07 18:02:05 -0500

    Tarea estados

--- 04-composición-componentes/composicion/src/App.js
+++ 04-composición-componentes/composicion/src/App.js
@@ -1,11 +1,11 @@
 import PostList from "./components/PostList";
 
-const App = () =>{
-    return(
-        <div>
-            <PostList/>
-        </div>
-    )
-}
+const App = () => {
+  return (
+    <div>
+      <PostList />
+    </div>
+  );
+};
 
 export default App;
--- 04-composición-componentes/composicion/src/components/PostList.js
+++ 04-composición-componentes/composicion/src/components/PostList.js
@@ -1,33 +1,47 @@
-
-import { results } from "./data/CardRick"
-
-import CardInfo from "./CardInfo"
-import styles from "./PostList.scss"
-
+import { results } from "./data/CardRick";
+import { useState } from "react";
+import CardInfo from "./CardInfo";
+import styles from "./PostList.scss";
 
 const PostList = () => {
-    return (
-        <div>
-            <h1 className={styles.h1}>The Rick and Morty API</h1>
-            <div>
-            {results.map((info)=>{
-                
-                return(
-                    <CardInfo 
-                    key={info.id} 
-                    name={info.name}
-                    status={info.status}
-                    species={info.species}
-                    origin={info.origin.name}
-                    location={info.location.name}
-                    image={info.image}
-                    />
+  const [status, setStatus] = useState("all");
 
-                )
-            })}
-            </div>
-        </div>
-    )
-}
+  const setAlive = () => setStatus("Alive");
 
-export default PostList
+  const setDead = () => setStatus("Dead");
+
+  const setUnknown = () => setStatus("unknown");
+
+  const setAll = () => setStatus("all");
+
+  return (
+    <div>
+      <h1 className={styles.h1}>The Rick and Morty API</h1>
+      <div>
+        <button onClick={setAlive}>Alive</button>
+        <button onClick={setDead}>Dead</button>
+        <button onClick={setUnknown}>Unknown</button>
+        <button onClick={setAll}>All</button>
+
+        {results.map((info) => {
+          if (info.status === status || status === "all") {
+            return (
+              <CardInfo
+                key={info.id}
+                name={info.name}
+                status={info.status}
+                species={info.species}
+                origin={info.origin.name}
+                location={info.location.name}
+                image={info.image}
+              />
+            );
+          }
+          return null;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default PostList;
